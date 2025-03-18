d7e16d64398af9ca04f32b29bea21bfc403ffc7a
BimaEkaSetiawan <bimaekasetiawan1010@gmail.com>
2025-03-18 15:31:31 +0700

    membuat progam perulangan naik/turun mengunakan FOR

---
+++ file1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //perulangan for untuk pecacah naik
+    int i;
+    cout << "Perulangan For Pencacah Naik" << endl;
+    for ( i = 0; i <= 4; i++)
+    {
+        cout << "Nilai i : " << i
+        << " Lari Keliling Lapangan" << endl;
+    }
+
+    cout << "Nilai i Terakhir : " << i << endl;
+    cout << endl;
+
+    //perulangan for pencacah turun
+    cout << "Perulangan For Pencacah Turun" << endl;
+    for (i = 4; i >= 0; i--)
+    {
+        cout << "Nilai i : " << i
+        << " Lari Keliling Lapangan" << endl;
+    }
+
+    cout << "Nilai i Terakhir : " << i << endl;
+    cout << endl;
+    
+    
+}
